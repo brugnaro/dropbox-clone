637b083f2960411c0fc1f60a6064bf044235107a
marcos <marcosrodriguesbrugnaro@gmail.com>
2018-10-11 23:26:03 -0300

    List files.

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -7,9 +7,10 @@ class DropBoxController {
 		this.progressBarEl = this.snackModalEl.querySelector('.mc-progress-bar-fg');
 		this.namefileEl = this.snackModalEl.querySelector('.filename');
 		this.timeleftEl = this.snackModalEl.querySelector('.timeleft');
-
+		this.listFilesEl = document.querySelector('#list-of-files-and-directories');
 		this.connectFirebase();
 		this.initEvents();
+		this.readFiles();
 	}
 
 	connectFirebase() {
@@ -336,13 +337,51 @@ class DropBoxController {
 		}
 	}
 
-	getFileView(file) {
-		return `
-		<li>
+	getFileView(file, key) {
+
+		let li = document.createElement('li');
+
+		li.dataset.key = key;
+
+		li.innerHTML = `
 			${this.getFileIconView(file)}
 			<div class="name text-center">${file.name}</div>
-		</li>
 		`;
+
+		this.initEventsLi(li);
+
+		return li;
+
+	}
+
+	readFiles() {
+
+		this.getFirebaseRef().on('value', snapshot => {
+
+			this.listFilesEl.innerHTML = '';
+
+			snapshot.forEach(snapshotItem => {
+
+				let key = snapshotItem.key;
+
+				let data = snapshotItem.val();
+
+				this.listFilesEl.appendChild(this.getFileView(data));
+
+			});
+
+		});
+
+	}
+
+	initEventsLi(li) {
+
+		li.addEventListener('click', e => {
+
+			li.classList.toggle('selected');
+
+		});
+
 	}
 
 }
