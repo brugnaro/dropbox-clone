591f0b0a53271c7010f4f4404ce86405b5ecfc6b
marcos <marcosrodriguesbrugnaro@gmail.com>
2018-11-01 20:06:53 -0300

    Add signup route

--- app/app.js
+++ app/app.js
@@ -5,7 +5,11 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var homeRouter = require('./routes/home');
 var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login');
+var signUpRouter = require('./routes/signup');
+
 
 var app = express();
 
@@ -20,15 +24,18 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/home', homeRouter);
 app.use('/users', usersRouter);
+app.use('/login', loginRouter);
+app.use('/signup', signUpRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
---
+++ app/public/src/login.js
@@ -0,0 +1,1 @@
+window.login = new LoginController();
